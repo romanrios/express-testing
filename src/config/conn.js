796138cb07811bafdc817572f9835882fb61bc83
Roman Rios <romanrios@live.com>
2023-12-20 19:22:09 -0300

    mysql1

--- src/config/conn.js
+++ src/config/conn.js
@@ -8,12 +8,6 @@ const db = serverless({
         user: process.env.DB_USER,
         password: process.env.DB_PASS,
     },
-    pool: {
-        min: 1,      // Número mínimo de conexiones en el pool
-        max: 5,      // Número máximo de conexiones en el pool
-        idleTimeoutMillis: 30000, // Tiempo máximo (en milisegundos) que una conexión puede estar inactiva antes de ser cerrada
-        acquireTimeoutMillis: 30000, // Tiempo máximo (en milisegundos) que una conexión puede estar esperando para ser adquirida del pool
-    },
 });
 
 module.exports = {
