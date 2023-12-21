e581c9663ff8237f45f0e3c80bcbca46c88e9032
Roman Rios <romanrios@live.com>
2023-12-20 22:10:05 -0300

    createConnection

--- src/models/productsModel.js
+++ src/models/productsModel.js
@@ -1,23 +1,10 @@
-const { conn } = require('../config/conn');
+const mysql = require('mysql2/promise')
+const connOb = require('../config/conn');
 
 module.exports = {
     getAll: async () => {
+        const conn = await mysql.createConnection(connOb)
         try {
-
-            // 'SELECT
-            //   products.*,
-            //   licences.licence_name,
-            //   categories.category_name
-            // FROM
-            //   products
-            // LEFT JOIN
-            //   licences ON products.licence_id = licences.licence_id
-            // LEFT JOIN
-            //   categories ON products.category_id = categories.category_id
-            // ORDER BY
-            //   product_id DESC;'
-
-
             const [rows] = await conn.query('SELECT products.*, licences.licence_name, categories.category_name FROM products LEFT JOIN licences ON products.licence_id = licences.licence_id LEFT JOIN categories ON products.category_id = categories.category_id ORDER BY product_id DESC;');
             return rows;
         } catch (error) {
